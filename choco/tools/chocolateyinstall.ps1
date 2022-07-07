e53f1dedf2d984f247776897feeb2ee8016482f2
david <dn200499@gmail.com>
2022-07-06 20:25:28 -0500

    Primer commit

---
+++ choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageName    = $env:ChocolateyPackageName
+$file           = "$($toolsDir)\theme.exe"
+$version        = "v1.2.0"
+$url            = "https://shopify-themekit.s3.amazonaws.com/$($version)/windows-386/theme.exe"
+$url64          = "https://shopify-themekit.s3.amazonaws.com/$($version)/windows-amd64/theme.exe"
+$checksum       = '827727f12600cdc8f248029c36486d43'
+$checksum64     = '7e407fe95e4124d1b3e99b66799f3fd9'
+$validExitCodes = @(0)
+
+Get-ChocolateyWebFile `
+  -PackageName $packageName `
+  -FileFullPath $file `
+  -Url "$url" `
+  -Checksum $checksum `
+  -ChecksumType "md5" `
+  -Url64bit "$url64" `
+  -Checksum64 $checksum64 `
+  -ChecksumType64 "md5"
